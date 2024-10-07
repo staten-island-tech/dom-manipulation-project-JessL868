9f2fe6190faecf36ec8e27b04b7065b66848fb58
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-10-07 14:00:01 -0400

    Project continue in 2 days

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const heading = document.querySelector("h1")
 
 const DOMSelectors = {
-    header: document.querySelector("h1")
+    header: document.querySelector("h1"),
+    items: document.querySelectorAll("li")
 }
