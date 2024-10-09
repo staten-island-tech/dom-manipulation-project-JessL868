8547e1b778a34b83b0a5c81a4da3107e4ce4fb72
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-10-09 13:59:05 -0400

    1 sec of code

--- index.js
+++ index.js
@@ -2,5 +2,9 @@ const heading = document.querySelector("h1")
 
 const DOMSelectors = {
     header: document.querySelector("h1"),
-    items: document.querySelectorAll("li")
+    items: document.querySelectorAll("li"),
+    cardHeader: document.querySelector(".card-header"),
+    button: document.querySelector(".btn"),
+    form:document.querySelector(".form"),
+    container: document.querySelector(".container")
 }
