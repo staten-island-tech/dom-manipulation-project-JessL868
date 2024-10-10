35aad0980ca020accc26286331058cf118d267ab
JessL868 <144480854+JessL868@users.noreply.github.com>
2024-10-10 13:57:18 -0400

    continued project

--- index.js
+++ index.js
@@ -2,9 +2,16 @@ const heading = document.querySelector("h1")
 
 const DOMSelectors = {
     header: document.querySelector("h1"),
-    items: document.querySelectorAll("li"),
+    items: document.querySelectorAll("ol"),
     cardHeader: document.querySelector(".card-header"),
     button: document.querySelector(".btn"),
     form:document.querySelector(".form"),
     container: document.querySelector(".container")
 }
+
+DOMSelectors.form.addEventListener("Submit", function(event){
+    event.preventDefault();
+    console.log(document.querySelector("input").value);
+});
+
+DOMSelectors.container.addEventListener("")
